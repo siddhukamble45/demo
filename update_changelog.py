cf239f96fe400c8950c0f335c70880c6d3f8fdb1
Siddheshwar Kamble <siddheshwar.kamble@privacera.com>
2024-08-16 18:32:33 +0530

    BUG: Fix flake issue in update_changelog script #12

--- update_changelog.py
+++ update_changelog.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 
 changelog_path = 'docs/CHANGELOG.md'
 
@@ -6,23 +8,26 @@ def get_current_version():
     with open('user_manager/VERSION') as file:
         for line in file:
             if line.startswith('__version__'):
-                # Extract the version value from the line
-                version = line.split('=')[1].strip().strip("'\"")
-                return version
+                version = line.split('=')[1]
+                version_no = version.strip().strip("'\"")
+                return version_no
     raise ValueError("Version not found")
 
 
 def update_changelog():
     version = get_current_version()
-
+    current_date = datetime.now().strftime('%d-%b-%Y')
     unreleased_header = f'## user-manager-{version} - [Unreleased]'
-    released_header = f'## user-manager-{version} - [Released]'
+    released_header = (f'## user-manager-{version} - '
+                       f'[Released] on {current_date}')
 
     with open(changelog_path, 'r') as file:
         changelog_content = file.read()
 
     # Replace the Unreleased header with the Released header
-    changelog_content = changelog_content.replace(unreleased_header, released_header)
+    changelog_content = changelog_content.replace(
+        unreleased_header, released_header
+    )
 
     with open(changelog_path, 'w') as file:
         file.write(changelog_content)
